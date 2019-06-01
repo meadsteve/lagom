e6076085a50268c7a683ea01cc0ff0660c10f02c
steve <meadsteve@gmail.com>
2019-06-01 16:29:07 +0200

    add example usage

---
+++ example.py
@@ -0,0 +1,43 @@
+from abc import ABC
+from dataclasses import dataclass
+
+from lagom import Container
+
+# Here is an example of some classes your application may be built from
+
+
+@dataclass
+class DiceApiUrl:
+    url: str
+
+
+class RateLimitingConfig:
+    pass
+
+
+class DiceClient(ABC):
+    pass
+
+
+class HttpDiceClient(DiceClient):
+
+    def __init__(self, url: DiceApiUrl, limiting: RateLimitingConfig):
+        pass
+
+
+class Game:
+    def __init__(self, dice_roller: DiceClient):
+        pass
+
+
+# Next we setup some definitions
+
+container = Container()
+# We need a specific url
+container[DiceApiUrl] = DiceApiUrl("https://roll.diceapi.com")
+# Wherever our code wants a DiceClient we get the http one
+container[DiceClient] = HttpDiceClient
+
+# Now the container can build the game object
+
+game = container[Game]
