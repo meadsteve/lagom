a7d72bdb675e905f9ea0c9b26535864df2fea47c
steve <meadsteve@gmail.com>
2019-06-02 13:28:55 +0200

    0.1.0 version with publishing fixed

--- lagom/__init__.py
+++ lagom/__init__.py
@@ -1,3 +1,7 @@
+"""Lagom, a type based dependency injection container"""
+
+__version__ = "0.1.0"
+
 from .definitions import Singleton, Construction, Alias
 from .container import Container
 from .decorators import bind_to_container
