b10de0be0bae4c3353d97a7b0bbe8442ec1e5774
steve <meadsteve@gmail.com>
2020-02-03 16:49:17 +0100

    update example

--- example.py
+++ example.py
@@ -22,6 +22,24 @@ class DiceClient(ABC):
     pass
 
 
+class PlayerDb:
+    def __init__(self, some_dsn):
+        pass
+
+    def load(self, name: str):
+        # Maybe cache what we've loaded. But for how long?
+        return "DATA"
+
+
+class LocalCachingPlayerDb(PlayerDb):
+    cache: typing.Dict
+
+    def load(self, name: str):
+        # cache what we've loaded FOR EVER
+        self.cache[name] = "CACHE"
+        return "DATA"
+
+
 class HttpDiceClient(DiceClient):
 
     def __init__(self, url: DiceApiUrl, limiting: RateLimitingConfig):
@@ -32,12 +50,20 @@ class Game:
     def __init__(self, dice_roller: DiceClient):
         pass
 
+    def make_move(self, a, b, move):
+        pass
+
 
 # Next we setup some definitions
 
 container = Container()
 # We need a specific url
 container[DiceApiUrl] = DiceApiUrl("https://roll.diceapi.com")
+
+# Our player DB should be reloaded each time as it caches locally
+# we'll share it per request later
+container[PlayerDb] = lambda: LocalCachingPlayerDb("blah blah blah")
+
 # Wherever our code wants a DiceClient we get the http one
 container[DiceClient] = HttpDiceClient
 
@@ -46,13 +72,14 @@ container[DiceClient] = HttpDiceClient
 game = container[Game]
 
 # If we wanted to build this into a web framework for example
-# we can partially bind functions to the container
-
-
-@bind_to_container(container)
-def handle_some_request(request: typing.Dict, game: Game):
-    # do something to the game
-    pass
+# we can partially bind functions to the container we share the
+# PlayerDB so the object can cache data locally but only for the
+# lifetime of a single request
+@bind_to_container(container, shared=[PlayerDb])
+def handle_some_request(request: typing.Dict, game: Game, player_db: PlayerDb):
+    player_one = player_db.load(request["player_one"])
+    player_two = player_db.load(request["player_two"])
+    game.make_move(player_one, player_two, request["move"])
 
 
 # we can now call the following. the game argument will automagically
