a8850ccbf2f456f448a10c52d4e4a26df28d0994
steve <meadsteve@gmail.com>
2020-02-03 14:55:32 +0100

    format

--- test/test_dep_definition_functions.py
+++ test/test_dep_definition_functions.py
@@ -2,9 +2,9 @@ from dataclasses import dataclass
 
 import pytest
 
-from lagom import Singleton, Construction, Container
+from lagom import Container
 from lagom.decorators import dependency_definition
-from lagom.exceptions import InvalidDependencyDefinition
+
 
 @dataclass
 class MyComplexDep:
@@ -12,7 +12,6 @@ class MyComplexDep:
 
 
 def test_functions_that_are_typed_can_be_used_by_a_container(container: Container):
-
     @dependency_definition(container)
     def my_constructor() -> MyComplexDep:
         return MyComplexDep(some_number=5)
@@ -23,6 +22,7 @@ def test_functions_that_are_typed_can_be_used_by_a_container(container: Containe
 def test_functions_that_are_not_typed_raise_an_error(container: Container):
 
     with pytest.raises(SyntaxError):
+
         @dependency_definition(container)
         def my_constructor():
             return MyComplexDep(some_number=5)
