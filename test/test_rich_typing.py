8b7240c742cae09ead3b180f7bd3c97047cf12fa
steve <meadsteve@gmail.com>
2020-02-05 08:38:06 +0100

    test protocols can be used

--- test/test_rich_typing.py
+++ test/test_rich_typing.py
@@ -1,5 +1,10 @@
 from typing import NewType, List
 
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol  # type: ignore
+
 from lagom import Container
 
 MyServiceUrl = NewType("MyServiceUrl", str)
@@ -15,6 +20,21 @@ class LoadBalancePerhaps:
         pass
 
 
+class BakerProtocol(Protocol):
+    def bake(self) -> str:
+        pass
+
+
+class Bakery:
+    def __init__(self, baker: BakerProtocol):
+        pass
+
+
+class CrumpetBaker:
+    def bake(self) -> str:
+        return "crumpets"
+
+
 def test_newtype_works_well_with_the_container(container: Container):
     container[MyServiceUrl] = MyServiceUrl("https://roll.diceapi.com")
     api_client = container[ApiClientOfSomeKind]
@@ -28,3 +48,10 @@ def test_lists_of_newtype_work_well_with_the_container(container: Container):
     ]
     load_balancer = container[LoadBalancePerhaps]
     assert isinstance(load_balancer, LoadBalancePerhaps)
+
+
+def test_protocols_work_well_with_the_container(container: Container):
+    # TODO: update contracts so this type ignore can be removed
+    container[BakerProtocol] = CrumpetBaker  # type: ignore
+    bakery = container[Bakery]
+    assert isinstance(bakery, Bakery)
