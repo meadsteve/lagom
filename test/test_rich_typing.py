51c58c0b561b241aa74b9b4b23466b222a79521a
steve <meadsteve@gmail.com>
2020-02-05 08:30:30 +0100

    test lists and NewType work well together

--- test/test_rich_typing.py
+++ test/test_rich_typing.py
@@ -1,4 +1,4 @@
-from typing import NewType
+from typing import NewType, List
 
 from lagom import Container
 
@@ -10,7 +10,21 @@ class ApiClientOfSomeKind:
         pass
 
 
+class LoadBalancePerhaps:
+    def __init__(self, urls: List[MyServiceUrl]):
+        pass
+
+
 def test_newtype_works_well_with_the_container(container: Container):
     container[MyServiceUrl] = MyServiceUrl("https://roll.diceapi.com")
     api_client = container[ApiClientOfSomeKind]
     assert isinstance(api_client, ApiClientOfSomeKind)
+
+
+def test_lists_of_newtype_work_well_with_the_container(container: Container):
+    container[List[MyServiceUrl]] = [
+        MyServiceUrl("https://roll.diceapi.com"),
+        MyServiceUrl("https://lol.diceapi.com"),
+    ]
+    load_balancer = container[LoadBalancePerhaps]
+    assert isinstance(load_balancer, LoadBalancePerhaps)
