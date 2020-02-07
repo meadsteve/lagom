7fa3864e67cb53de01bf85c02ac7165156b5d34f
steve <meadsteve@gmail.com>
2020-02-07 10:02:10 +0100

    add untested django integration (it's had manual testing in django project)

---
+++ lagom/integrations/django.py
@@ -0,0 +1,44 @@
+import types
+from typing import TypeVar, Generic
+
+from django.db.models import Manager
+from django.views import View
+
+from lagom import Container
+
+M = TypeVar("M")
+
+
+class DjangoModel(Generic[M]):
+    model: M
+    objects: Manager
+
+    def __init__(self, model: M):
+        self.model = model
+        self.objects = model.objects
+
+    def new(self, **kwargs) -> M:
+        return self.model(**kwargs)
+
+
+class DjangoContainer(Container):
+    def __init__(self, models=None, container: Container = None):
+        super().__init__(container)
+        for model in models or []:
+            self[DjangoModel[model]] = lambda: DjangoModel(model)
+
+    def view(self, view):
+        if isinstance(view, types.FunctionType):
+            # Plain old function can be bound to the container
+            return self.partial(view)
+        if issubclass(view, View):
+            # For django class based view each method needs to be bound
+            # to the container
+            self._bind_view_methods_to_container(view)
+            return view.as_view()
+        raise SyntaxError(f"Container doesn't know how to handle type {type(view)}")
+
+    def _bind_view_methods_to_container(self, view):
+        for method in View.http_method_names:
+            if hasattr(view, method):
+                setattr(view, method, self.partial(getattr(view, method)))
