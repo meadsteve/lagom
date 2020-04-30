860b4fe71824f533b02a4af429eb372aaffb708b
steve <meadsteve@gmail.com>
2020-04-30 15:40:07 +0200

    decorator docs

--- lagom/decorators.py
+++ lagom/decorators.py
@@ -1,3 +1,7 @@
+"""
+This module provides decorators for hooking an
+application into the container.s
+"""
 import inspect
 from typing import List, Type
 
@@ -7,6 +11,17 @@ from .util.reflection import RETURN_ANNOTATION
 
 
 def bind_to_container(container: Container, shared: List[Type] = None):
+    """Decorates the function so that it's uses the container to construct things
+
+    >>> from tests.examples import SomeClass
+    >>> c = Container()
+    >>> @bind_to_container(c)
+    ... def say_hello_from(sayer: SomeClass):
+    ...    return f"hello from {sayer}"
+    >>> say_hello_from()
+    'hello from <tests.examples.SomeClass object at ...>'
+
+    """
     def decorator(func):
         return container.partial(func, shared=shared)
 
@@ -14,6 +29,19 @@ def bind_to_container(container: Container, shared: List[Type] = None):
 
 
 def dependency_definition(container: Container, singleton: bool = False):
+    """ Registers the provided function with the container
+    The return type of the decorated function will be reflected and whenever
+    the container is asked for this type the function will be called
+
+    >>> from tests.examples import SomeClass, SomeExtendedClass
+    >>> c = Container()
+    >>> @dependency_definition(c)
+    ... def build_some_class_but_extended() -> SomeClass:
+    ...    return SomeExtendedClass()
+    >>> c.resolve(SomeClass)
+    <tests.examples.SomeExtendedClass object at ...>
+
+    """
     def decorator(func):
         try:
             arg_spec = inspect.getfullargspec(func)
