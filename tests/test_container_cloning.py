ac71f2998d3baa610bc294e8e85553c9f04d888d
steve <meadsteve@gmail.com>
2020-05-31 20:16:36 +0200

    test singletons in cloned containers fixes #45

--- tests/test_container_cloning.py
+++ tests/test_container_cloning.py
@@ -1,7 +1,4 @@
-import pytest
-
-from lagom import Construction, Container
-from lagom.exceptions import DuplicateDefinition
+from lagom import Construction, Container, Singleton
 
 
 class InitialDep:
@@ -30,3 +27,18 @@ def test_a_cloned_container_can_have_deps_overwritten(container: Container):
     new_container.define(InitialDep, Construction(lambda: SomeOtherMockForTesting()))
 
     assert isinstance(new_container[InitialDep], SomeOtherMockForTesting)
+
+
+def test_a_clone_shares_the_parents_singleton_instances(container: Container):
+    container.define(InitialDep, Singleton(InitialDep))
+    new_container = container.clone()
+
+    assert id(container[InitialDep]) == id(new_container[InitialDep])
+
+
+def test_overwriting_a_singleton_creates_a_new_one(container: Container):
+    container.define(InitialDep, Singleton(InitialDep))
+    new_container = container.clone()
+    new_container.define(InitialDep, Singleton(InitialDep))
+
+    assert id(container[InitialDep]) != id(new_container[InitialDep])
