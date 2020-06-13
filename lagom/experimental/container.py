6252525791adc088e4797d61df0a6a2e2ec90c64
steve <meadsteve@gmail.com>
2020-06-13 16:26:56 +0200

    wip2

--- lagom/experimental/container.py
+++ lagom/experimental/container.py
@@ -1,4 +1,4 @@
-from typing import Type, TypeVar
+from typing import Type, TypeVar, Set
 
 from .. import Container
 from .exceptions import DependencyNotDefined
@@ -7,9 +7,13 @@ X = TypeVar("X")
 
 
 class ExplicitContainer(Container):
-    def resolve(
-        self, dep_type: Type[X], suppress_error=False, skip_definitions=False
-    ) -> X:
+    def _resolve(
+        self,
+        dep_type: Type[X],
+        suppress_error=False,
+        skip_definitions=False,
+        types_to_skip: Set[Type] = None,
+    ):
         if dep_type not in self.defined_types:
             if suppress_error:
                 return None  # type: ignore
@@ -17,4 +21,6 @@ class ExplicitContainer(Container):
                 f"{dep_type.__name__} has not been defined. "
                 "In an explict container all dependencies must be defined"
             )
-        return super().resolve(dep_type, suppress_error, skip_definitions)
+        return super()._resolve(
+            dep_type, suppress_error, skip_definitions, types_to_skip
+        )
