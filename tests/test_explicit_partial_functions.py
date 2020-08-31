31ae7cbd80584f007cb2168fba3c7a33b5c81c15
steve <meadsteve@gmail.com>
2020-08-31 14:47:57 +0200

    test explicit injected arguments can be overridden

--- tests/test_explicit_partial_functions.py
+++ tests/test_explicit_partial_functions.py
@@ -27,6 +27,10 @@ def test_functions_decorated_get_the_correct_argument():
     assert example_function(message=" world") == "testing world"
 
 
+def test_injected_arguments_can_over_overridden():
+    assert example_function(message=" world",  resolved=MyDep("set")) == "set world"
+
+
 @pytest.mark.asyncio
 async def test_async_functions_decorated_get_the_correct_argument():
     assert await async_example_function(message=" world") == "testing world"
