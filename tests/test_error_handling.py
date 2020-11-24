0daf68b9c8f02bf1a311bbd5e54b4eef7fb2bc71
steve <meadsteve@gmail.com>
2020-11-24 17:19:52 +0100

    re-enable broken test

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -56,7 +56,6 @@ class B:
         pass
 
 
-@pytest.mark.skip(reason="fix me later")
 def test_circular_imports_raise_a_clear_error(container):
     with pytest.raises(RecursiveDefinitionError) as e_info:
         container.resolve(A)
