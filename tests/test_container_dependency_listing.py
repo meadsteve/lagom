9d41e848f72c3b87b21d9429c789fba10067f809
steve <meadsteve@gmail.com>
2020-11-26 17:40:17 +0100

    whoops forgot the test in the last thing

--- tests/test_container_dependency_listing.py
+++ tests/test_container_dependency_listing.py
@@ -21,3 +21,24 @@ def test_container_can_list_the_types_explicitly_defined(container: Container):
         Optional[InitialDep],
         Optional[SomeOtherThing],
     }
+
+
+def test_container_can_list_the_types_explicitly_defined_in_a_cloned_container(
+    container: Container,
+):
+    container[InitialDep] = InitialDep
+
+    child_container = container.clone()
+    child_container[SomeOtherThing] = Singleton(SomeOtherThing)
+
+    assert child_container.defined_types == {
+        InitialDep,
+        SomeOtherThing,
+        Optional[InitialDep],
+        Optional[SomeOtherThing],
+    }
+
+    assert container.defined_types == {
+        InitialDep,
+        Optional[InitialDep],
+    }
