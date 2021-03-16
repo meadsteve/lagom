ad6ffddec8090b9036d3d8ead39c034a70b45e1a
steve <steve@meadsteve.dev>
2021-03-16 19:52:35 +0100

    WIP

--- lagom/experimental/integrations/click.py
+++ lagom/experimental/integrations/click.py
@@ -9,20 +9,21 @@ class ClickIO:
     """
     Provides an injectable hint for click IO if needed.
     """
+
     @staticmethod
     def echo(message=None, file=None, nl=True, err=False, color=None):
         utils.echo(message, file, nl, err, color)
 
     def __getattr__(self, item):
         import click
+
         return getattr(click, item)
 
 
 class ClickIntegration:
     """
-    Integration between a container and the FastAPI framework.
-    Provides a method `Depends` which functions in the same way as
-    FastApi `Depends`
+    Integration between a container and click. The ClickIntegration
+    instance provides all the same decorators and functions as click.
     """
 
     _container: WriteableContainer
@@ -37,6 +38,16 @@ class ClickIntegration:
         self._execution_singletons = execution_singletons or []
 
     def command(self, name=None, cls=None, **attrs):
+        """
+        Proxies click.command but binds the function to lagom
+        so that any arguments with lagom.injectable as a default will
+        be injected by the container
+        :param name:
+        :param cls:
+        :param attrs:
+        :return:
+        """
+
         def _decorator(f):
             bound_f = self._container.partial(f, shared=self._execution_singletons)
             return decorators.command(name, cls, **attrs)(bound_f)
@@ -45,9 +56,16 @@ class ClickIntegration:
 
     @staticmethod
     def option(*param_decls, **attrs):
+        """
+        Proxies click.option
+        :param param_decls:
+        :param attrs:
+        :return:
+        """
         return decorators.option(*param_decls, **attrs)
 
     def __getattr__(self, item):
         # Any method not explicitly code just gets passed to click
         import click
+
         return getattr(click, item)
