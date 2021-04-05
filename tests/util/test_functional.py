c5729bafc13e90621cd7472984ce50e6091c071d
steve <steve@meadsteve.dev>
2021-04-05 11:17:40 +0200

    Add FunctionCollection as a type

--- tests/util/test_functional.py
+++ tests/util/test_functional.py
@@ -1,6 +1,14 @@
 import pytest
 
-from lagom.util.functional import arity
+from lagom.util.functional import arity, FunctionCollection
+
+
+def _func_a():
+    pass
+
+
+def _func_b():
+    pass
 
 
 @pytest.mark.parametrize(
@@ -9,3 +17,14 @@ from lagom.util.functional import arity
 )
 def test_we_can_get_arity_from_functions(test_func, expected_arity: int):
     assert arity(test_func) == expected_arity
+
+
+def test_function_collections_are_the_same_if_they_have_the_same_functions():
+    assert FunctionCollection(_func_a, _func_b) == FunctionCollection(_func_a, _func_b)
+
+
+def test_function_collections_are_iterable():
+    collected_funcs = []
+    for func in FunctionCollection(_func_a, _func_b):
+        collected_funcs.append(func)
+    assert collected_funcs == [_func_a, _func_b]
