cb98769837111de852b55b7a6f9847f602b4d532
steve <steve@meadsteve.dev>
2021-04-06 19:59:45 +0200

    wip

--- tests/test_function_collections.py
+++ tests/test_function_collections.py
@@ -13,4 +13,4 @@ def _func_b(input: str) -> str:
 
 def test_function_collections_are_built_from_lists_of_funcs(container: Container):
     container[Collection[Callable[[str], str]]] = [_func_b, _func_a]  # type: ignore # sadly collection still can't be used like this
-    assert type(container[Collection[Callable[[str], str]]]) == FunctionCollection # type: ignore # sadly collection still can't be used like this
+    assert type(container[Collection[Callable[[str], str]]]) == FunctionCollection  # type: ignore # sadly collection still can't be used like this
