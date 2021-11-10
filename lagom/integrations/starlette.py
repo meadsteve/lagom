247b73ec287bcde4bedcca21987a00f13174f2b9
Tom Keefe <8655118+MisterKeefe@users.noreply.github.com>
2021-11-10 07:47:40 +0000

    handle injecting into starlette HTTPEndpoints + tests (#170)
    * handle injecting into starlette HTTPEndpoints + tests
    
    * Fix narrowing type error
    
    * blacken code
    
    * change allowlist to HTTP methods rather than starlette internals

--- lagom/integrations/starlette.py
+++ lagom/integrations/starlette.py
@@ -1,12 +1,24 @@
 """
 For use with Starlette (https://www.starlette.io/)
 """
-from typing import List, Type, Callable, Optional
+from typing import List, Type, Callable, Optional, Union
 
 from starlette.routing import Route
-
+from starlette.endpoints import HTTPEndpoint
 
 from ..interfaces import ExtendableContainer
+
+OVERRIDE_HTTP_METHODS = {
+    "get",
+    "head",
+    "post",
+    "put",
+    "delete",
+    "connect",
+    "options",
+    "trace",
+    "patch",
+}
 
 
 class StarletteIntegration:
@@ -48,12 +60,11 @@ class StarletteIntegration:
         :param include_in_schema:
         :return:
         """
-        wrapped_endpoint = self._container.partial(
-            endpoint, shared=self._request_singletons
-        )
+        wrapped = self.wrapped_endpoint_factory(endpoint, self._container.partial)
+
         return Route(
             path,
-            wrapped_endpoint,
+            wrapped,
             methods=methods,
             name=name,
             include_in_schema=include_in_schema,
@@ -78,13 +89,59 @@ class StarletteIntegration:
         :param include_in_schema:
         :return:
         """
-        wrapped_endpoint = self._container.magic_partial(
-            endpoint, shared=self._request_singletons
-        )
+        wrapped = self.wrapped_endpoint_factory(endpoint, self._container.magic_partial)
+
         return Route(
             path,
-            wrapped_endpoint,
+            wrapped,
             methods=methods,
             name=name,
             include_in_schema=include_in_schema,
         )
+
+    def wrapped_endpoint_factory(
+        self, endpoint: Union[Callable, Type[HTTPEndpoint]], partial_provider: Callable
+    ):
+        """Builds an instance of a starlette Route with endpoint callables
+        bound to the container so dependencies can be auto injected.
+
+        :param endpoint:
+        :param partial_provider:
+        """
+        if not isinstance(endpoint, type):
+            return partial_provider(endpoint, shared=self._request_singletons)
+
+        if issubclass(endpoint, HTTPEndpoint):
+            return self.create_http_endpoint_proxy(
+                endpoint, partial_provider, self._request_singletons
+            )
+
+    @staticmethod
+    def create_http_endpoint_proxy(
+        endpoint_cls: Type[HTTPEndpoint],
+        partial_provider: Callable,
+        request_singletons: List[Type],
+    ) -> Type[HTTPEndpoint]:
+        """Create a subclass of Starlette's HTTPEndpoint which injects dependencies
+        into HTTP-method-named methods on the user's `endpoint_cls` subclass of HTTPEndpoint
+
+        :param endpoint_cls:
+        :param partial_provider:
+        :param request_singletons:
+        """
+
+        class HTTPEndpointProxy(HTTPEndpoint):
+            def __init__(self, scope, receive, send):
+                super().__init__(scope, receive, send)
+                self.endpoint = endpoint_cls(scope, receive, send)
+
+            def __getattribute__(self, name: str):
+                if name in OVERRIDE_HTTP_METHODS:
+                    endpoint_instance = object.__getattribute__(self, "endpoint")
+                    endpoint_method = getattr(endpoint_instance, name)
+
+                    return partial_provider(endpoint_method, shared=request_singletons)
+
+                return object.__getattribute__(self, name)
+
+        return HTTPEndpointProxy
--- tests/integrations/test_starlette.py
+++ tests/integrations/test_starlette.py
@@ -1,6 +1,11 @@
-from starlette.routing import Route
+import pytest
 
-from lagom import Container, injectable
+from starlette.routing import Route
+from starlette.endpoints import HTTPEndpoint
+from starlette.responses import PlainTextResponse
+from starlette.testclient import TestClient
+
+from lagom import injectable
 from lagom.integrations.starlette import StarletteIntegration
 
 
@@ -21,6 +26,15 @@ def two_dep_handler(request, dep_one: MyDep = injectable, dep_two: MyDep = injec
     return "singleton" if dep_one is dep_two else "new instances"
 
 
+async def some_async_handler(request, dep: MyDep = injectable):
+    return "ok"
+
+
+class SomeEndpointHandler(HTTPEndpoint):
+    async def get(self, request, dep: MyDep = injectable):
+        return PlainTextResponse("ok")
+
+
 def test_a_special_starlette_container_can_be_used_and_provides_routes(container):
     sc = StarletteIntegration(container)
     route = sc.route("/", some_handler)
@@ -32,3 +46,26 @@ def test_the_starlette_container_can_define_request_level_singletons(container):
     sc = StarletteIntegration(container, request_singletons=[MyDep])
     route = sc.route("/two", two_dep_handler)
     assert route.endpoint({}) == "singleton"
+
+
+@pytest.mark.asyncio
+async def test_the_starlette_container_handles_async_handlers(container):
+    sc = StarletteIntegration(container, request_singletons=[MyDep])
+
+    route = sc.route("/", some_async_handler)
+
+    assert isinstance(route, Route)
+    assert await route.endpoint({}) == "ok"
+
+
+@pytest.mark.asyncio
+async def test_the_starlette_container_can_handle_endpoint_classes(container):
+    sc = StarletteIntegration(container)
+    route = sc.route("/", SomeEndpointHandler)
+    assert isinstance(route, Route)
+
+    client = TestClient(route)
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.text == "ok"
