8bd5e3d49ea2e7ae44b20931ccb4aeff73bc22f9
steve <steve@meadsteve.dev>
2021-11-19 15:10:19 +0100

    test temporary_singletons returns a reusable context

--- tests/test_temporary_singletons.py
+++ tests/test_temporary_singletons.py
@@ -19,3 +19,15 @@ def test_temporary_singletons_dont_effect_the_base_container(container: Containe
     with container.temporary_singletons([SomeDep]) as container_with_singletons:
         the_singleton = container_with_singletons[SomeDep]
     assert container[SomeDep] is not the_singleton
+
+
+def test_temporary_singletons_context_is_reusable_but_doesnt_share_state(container: Container):
+    context = container.temporary_singletons([SomeDep])
+
+    with context as c1:
+        first = c1[SomeDep]
+
+    with context as c2:
+        second = c2[SomeDep]
+
+    assert first is not second
