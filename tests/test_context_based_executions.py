344d548264d32a142477557079adb8ce3a8324a7
steve <steve@meadsteve.dev>
2021-11-27 13:18:59 +0100

    test the context manager unwinding happens recursively

--- tests/test_context_based_executions.py
+++ tests/test_context_based_executions.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Iterator
+from typing import Iterator, Generator
 
 from context_based import ContextContainer
 from lagom import Container, dependency_definition
@@ -9,16 +9,32 @@ class SomeDep:
     global_clean_up_has_happened = False
 
 
+class SomeWrapperDep:
+    global_clean_up_has_happened = False
+
+    def __init__(self, dep: SomeDep):
+        pass
+
+
 container = Container()
 
 
 @dependency_definition(container)
 @contextmanager
-def _load_a_some_dep_then_clean(c) -> Iterator[SomeDep]:
+def _load_a_some_dep_then_clean() -> Generator[SomeDep, None, None]:
     try:
         yield SomeDep()
     finally:
         SomeDep.global_clean_up_has_happened = True
+
+
+@dependency_definition(container)
+@contextmanager
+def _load_a_some_wrapper_dep_then_clean(c) -> Iterator[SomeWrapperDep]:
+    try:
+        yield SomeWrapperDep(c[SomeDep])
+    finally:
+        SomeWrapperDep.global_clean_up_has_happened = True
 
 
 def test_clean_up_of_loaded_contexts_happens_on_container_exit():
@@ -28,3 +44,15 @@ def test_clean_up_of_loaded_contexts_happens_on_container_exit():
         assert isinstance(context_container[SomeDep], SomeDep)
         assert not SomeDep.global_clean_up_has_happened
     assert SomeDep.global_clean_up_has_happened
+
+
+def test_clean_up_of_loaded_contexts_happens_recursively_on_container_exit():
+    SomeDep.global_clean_up_has_happened = False
+
+    with ContextContainer(container, context_types=[SomeDep, SomeWrapperDep]) as context_container:
+        assert isinstance(context_container[SomeWrapperDep], SomeWrapperDep)
+        assert not SomeDep.global_clean_up_has_happened
+        assert not SomeWrapperDep.global_clean_up_has_happened
+
+    assert SomeDep.global_clean_up_has_happened
+    assert SomeWrapperDep.global_clean_up_has_happened
